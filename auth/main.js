d9131f1ee70f39eaf46b240c9b45ab599b5620cf
Susovan Dev <susovandas.dev@gmail.com>
2025-10-18 09:37:51 +0530

    feat(auth-service): integrate database connection for auth microservic

--- auth/main.js
+++ auth/main.js
@@ -1,9 +1,17 @@
 import app from './src/app.js';
 import { config } from './src/config/env.config.js';
+import { connectDB } from './src/db/db.js';
 
 const PORT = config.SERVER.PORT;
 const HOST = config.SERVER.HOST;
 const PROTOCOL = config.SERVER.PROTOCOL;
-app.listen(PORT, () => {
-  console.log(`Server running on ${PROTOCOL}://${HOST}:${PORT}`);
-});
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PROTOCOL}://${HOST}:${PORT}`);
+    });
+  })
+  .catch(error =>
+    console.error(`Database connection error while starting server: ${error}`)
+  );
--- auth/src/config/env.config.js
+++ auth/src/config/env.config.js
@@ -1,12 +1,15 @@
-import dotenv from "dotenv";
-dotenv.config("../../.env");
+import dotenv from 'dotenv';
+dotenv.config('../../.env');
 
 const _config = {
   SERVER: {
     PORT: process.env.PORT || 4002,
-    HOST: process.env.HOST || "localhost",
-    ENV: process.env.NODE_ENV || "development",
-    PROTOCOL: process.env.PROTOCOL || "http",
+    HOST: process.env.HOST || 'localhost',
+    ENV: process.env.NODE_ENV || 'development',
+    PROTOCOL: process.env.PROTOCOL || 'http',
+  },
+  DATABASE: {
+    URL: process.env.DATABASE_URL,
   },
 };
 
